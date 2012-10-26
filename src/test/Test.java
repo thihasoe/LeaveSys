2719d1be6af35b3f7f52ceb0cfe5027ed320cd80
thihasoe <thihasoe@THIHASOE9D59>
2012-10-26 22:56:25 +0800

    test project

---
+++ src/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import com.sun.xml.internal.ws.message.saaj.SAAJHeader;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+/**
+ *
+ * @author thihasoe
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello");
+    }
+}
